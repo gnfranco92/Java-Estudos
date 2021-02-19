f5ba5318512f9936d20f8ec74d662f15c3610b43
Guilherme Neto Franco <guilhermenetofranco@ymail.com>
2021-02-18 23:39:55 -0300

    Commit scan

---
+++ Exercicios/pega_valor_retorna.java
@@ -0,0 +1,17 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class pega_valor_retorna {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print5("Digite um valor inteiro: ");
+        int n = scan.nextInt();
+
+        System.out.println("Número digitado: "+n);
+
+    }
+    
+}
