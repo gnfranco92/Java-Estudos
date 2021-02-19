58812795d8684cd700bc434d0a5ae9f4210b0fb5
Guilherme Neto Franco <guilhermenetofranco@ymail.com>
2021-02-18 21:16:37 -0300

    operadores && e ||

---
+++ operador_logico.java
@@ -0,0 +1,17 @@
+public class operador_logico {
+
+    public static void main(String[] args) {
+        //&& = e
+        // || = ou
+        // Condições baseadas na tabela verdade
+
+        if(10 == 10 && 10 <= 20){
+            System.out.println("Dentro do if");
+
+        }else{
+            System.out.println("Dentro do else");
+
+        }
+    }
+    
+}
