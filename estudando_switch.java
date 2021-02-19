d8bc6ae122c039cd0312f3efb8b06e5614945698
Guilherme Neto Franco <guilhermenetofranco@ymail.com>
2021-02-18 23:10:31 -0300

    Switch

---
+++ estudando_switch.java
@@ -0,0 +1,18 @@
+import java.security.Principal;
+
+public class estudando_switch {
+    public static void main(String[] args) {
+        int op = 0;
+
+        switch (op) {
+            case 1:
+                System.out.println("Primeira opcao.");                
+                break;
+        
+            default:
+                System.out.println("Escolha uma opcao.");
+                break;
+        }        
+    }
+    
+}
